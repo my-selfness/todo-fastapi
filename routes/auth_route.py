cd188c562b86ccb7228dc7b8fa679c78743edc16
my-selfness <suarkata1@gmail.com>
2024-08-08 01:43:42 +0530

    All Authentication done

--- routes/auth_route.py
+++ routes/auth_route.py
@@ -57,7 +57,6 @@ async def login_for_access_token(
     access_token = create_access_token(
         data={"sub": user["email"]}, expires_delta=access_token_expires
     )
-
     return Token(access_token=access_token, token_type="bearer")
 
 
